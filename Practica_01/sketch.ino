6ec2901e7923180acab0f5a119731c0284256913
Hugo Méndez-Guzmán <86522858+hmendezg21@users.noreply.github.com>
2026-09-03 19:38:29 -0600

    Implement counter in loop to print values
    Added a counter that prints values from 0 to 10 in the loop.

--- Practica_01/sketch.ino
+++ Practica_01/sketch.ino
@@ -1,10 +1,19 @@
+int contador;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(contador=0;contador<=10; contador++){
+    Serial.print("Contador:");
+    Serial.println(contador);
+  }
+
+
   delay(10); // this speeds up the simulation
 }
